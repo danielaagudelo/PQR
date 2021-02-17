630eb1ce863420325d0f42eebf0a58fb9dda8ffb
Daniela Agudelo <danyela123@hotmail.es>
2021-02-16 20:42:03 -0500

    Nueva funcionalidad

---
+++ funcion.php
@@ -0,0 +1,14 @@
+<?php
+class HolaMundo
+{
+private $nombre;
+function __construct($nombre)
+{
+31
+$this->nombre = $nombre;
+}
+function __toString()
+{
+return sprintf ("Hola, %s.\n", $this->nombre);
+}
+}
--- holamundo.php
+++ holamundo.php
@@ -1,3 +1,4 @@
 <?php
 echo "Hola Mundo{$argv[1]}\n";
 echo "Hola Mundo2{$argv[1]}\n";
+print new "funcion($nombre)\n";
